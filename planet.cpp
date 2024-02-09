255f34f432a91e0bcefe3041d0223c40c3e42086
jedizombiekiller <53279920+jedizombiekiller@users.noreply.github.com>
2024-02-09 14:27:21 +0000

    Added Module 6 assignments

---
+++ planet.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double celcius;
+    double fahrenheit;
+
+    cout << "What temperature is it in celcius?" << endl;
+    cin >> celcius;
+    fahrenheit = (celcius*(9/5.0)) + 32;
+    cout << "Your temperature in fahrenheit is " << fahrenheit >> " degrees" << endl;
+}
